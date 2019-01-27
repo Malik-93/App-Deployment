719f35ad04c1c5a166ee9493306b7475c711d951
Mudassir Malik <44253169+Malik-93@users.noreply.github.com>
2019-01-27 20:17:21 +0500

    final decision

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-var db_url = process.env.DB_URL || 'mongodb://localhost:27017/Server-DB'
+var db_url = 'mongodb://Mudassir_Malik:mLab147800.mlab.com:13855/mongo-users'
 mongoose.connect(db_url, { useNewUrlParser: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
